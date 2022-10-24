2d8be38ae59b80c1cd0acaced5906c5287b2dbe7
Ulugbek-Akmalovich-Shodimurodov <ulugbekshodimurodov16@gmail.com>
2022-10-24 13:32:20 +0500

    FINISH CODE

--- js/main.js
+++ js/main.js
@@ -5,19 +5,22 @@ let modalBtn = document.querySelector('.modal-btn');
 let modalTitle = document.querySelector('.modal-title');
 let navbarOpenBtn = document.querySelector('.site-navbar__bars')
 let navbarList = document.querySelector('.site__navbar-list')
+let modalClose = document.querySelector('.modal-container')
 
 function openModal(){
     popup.classList.add('open-popup');
-    openMContainer.classList.add('modal-container-open');
+    openMContainer.classList.add('modal-container__event');
 }
 function closeModal(){
     popup.classList.remove('open-popup');
     openMContainer.classList.remove('modal-container-open');
+    modalClose.classList.remove('modal-container__event');
 }
 function findNumber(e){
     modalTitle.textContent = `${e.target.textContent}`;
     popup.classList.add('open-popup');
     openModal();         
+    openMContainer.classList.add('modal-container-open');
 }
 function navbarOpen(){
     navbarList.classList.toggle('navbar__list-open')
@@ -25,4 +28,5 @@ function navbarOpen(){
 
 cardList.addEventListener('click', findNumber);
 modalBtn.addEventListener('click', closeModal);
+modalClose.addEventListener('click', closeModal);
 navbarOpenBtn.addEventListener('click', navbarOpen);
